37c8c6c4e921f291e002da6f60c973a09d655815
nawszin <kkowalskigustavo@gmail.com>
2025-07-24 20:39:31 -0300

    FindById testes integração com RestAssured

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -11,11 +11,14 @@ import static org.hamcrest.Matchers.*;
 public class MovieControllerRA {
 
     private String titleName;
-
+    private String existingId, nonExistingId;
     @BeforeEach
     void setUp(){
         titleName = "Guerra Mundial Z";
         baseURI = "http://localhost:8080";
+
+        existingId = "18";
+        nonExistingId = "200";
     }
 
     @Test
@@ -37,6 +40,27 @@ public class MovieControllerRA {
                 .body("content.title", hasItems("Guerra Mundial Z"))
                 .body("content.score", hasItem(0.0f))
                 .body("content", hasSize(1));
-        ;
     }
+    @Test
+    public void findByIdShouldReturnMovieWhenIdExists(){
+        given()
+                .get("/movies/{existingId}", existingId)
+                .then()
+                .statusCode(200)
+                .body("id", is(18))
+                .body("title", equalTo("Guerra Mundial Z"))
+                .body("score", is(0.0f))
+                .body("count", is(0))
+                .body("image", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/31VpBgUX5O4Z3dn5ZbX8HLqoXH3.jpg"));
+    }
+    @Test
+    public void findByIdShouldReturnNotFoundWhenIdDoesNotExist(){
+        given()
+                .get("/movies/{nonExistingId}", nonExistingId)
+                .then()
+                .statusCode(404)
+                .body("path", equalTo("/movies/200"))
+                .body("error", equalTo("Recurso não encontrado"));
+    }
+
 }
